3a5fcb5272cb45457d9f95b1cb369e1818ff393e
Tamara Ziswiler <ziswt1@bfh.ch>
2024-11-13 13:14:43 +0100

    JS Fetch funktioniert, bearer token muss regelmässig angepasst werden.

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Portfolio-Website geladen!");
 });
+
+// Abruf der API und Anzeige der Daten
+fetch(
+  "https://api.srgssr.ch/srf-meteo/v2/geolocationNames?name=sillerenb%C3%BChl",
+  {
+    method: "GET", // oder GET, PUT, DELETE
+    headers: {
+      Authorization: "Bearer kbAGq1w3AtZY9M6RGyIIdA3x9Aj9",
+      Accept: "application/json",
+    },
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const locationsDiv = document.getElementById("locations");
+
+    data.forEach((location) => {
+      const locationDiv = document.createElement("div");
+      locationDiv.classList.add("location");
+
+      locationDiv.innerHTML = `
+                  <h2>${location.name}</h2>
+                  <p><strong>Höhe:</strong> ${location.height} m ü.M.</p>
+                  <p><strong>Geolocation:</strong> [${location.geolocation.lat}, ${location.geolocation.lon}]</p>
+                  <p><strong>Geolocation:</strong> ${location.geolocation.id}</p>
+              `;
+
+      // Schleife durch die geolocation_names für weitere Orte
+      location.geolocation.geolocation_names.forEach((geoName) => {
+        const geoNameDiv = document.createElement("div");
+        geoNameDiv.classList.add("geolocation-name");
+        geoNameDiv.innerHTML = `
+                      <p>${geoName.name} (${geoName.plz}), ${geoName.province}, ${geoName.height} m ü.M.</p>
+                  `;
+        locationDiv.appendChild(geoNameDiv);
+      });
+
+      locationsDiv.appendChild(locationDiv);
+    });
+  })
+  .catch((error) => {
+    console.error("Es gab ein Problem mit der API-Anfrage:", error);
+  });
