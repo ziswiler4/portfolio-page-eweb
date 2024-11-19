5948cbafbc71bae7c3f4bdd97772d80eb5679234
Tamara Ziswiler <ziswt1@bfh.ch>
2024-11-19 21:13:12 +0100

    finalisierung wetter api

--- script.js
+++ script.js
@@ -44,13 +44,11 @@ fetch("https://api.srgssr.ch/srf-meteo/v2/forecastpoint/46.4716%2C7.5181", {
 
         const weatherDiv = document.getElementById("weather");
         weatherDiv.innerHTML = `
-          <p><strong>Höhe:</strong> 1978 m. ü. M.</p>   
           <p><strong>Min. Temperatur:</strong> ${TTL_C}°C</p>
           <p><strong>Max. Temperatur:</strong> ${TTH_C}°C</p>
           <p><strong>Gefühlte Temperatur:</strong> ${TTTFEEL_C}°C</p>
           <p><strong>Windgeschwindigkeit:</strong> ${FF_KMH} km/h</p>
           <p><strong>Neuschnee:</strong> ${FRESHSNOW_MM} mm</p>
-          <p><strong>Sonnenstunden:</strong> ${SUN_MIN}</p>
         `;
       } else {
         console.error("Keine Wetterdaten für den heutigen Tag gefunden.");
@@ -62,46 +60,3 @@ fetch("https://api.srgssr.ch/srf-meteo/v2/forecastpoint/46.4716%2C7.5181", {
   .catch((error) => {
     console.error("Fehler bei der API-Anfrage:", error);
   });
-
-/* Abruf der API und Anzeige der Daten
-fetch(
-  "https://api.srgssr.ch/srf-meteo/v2/geolocationNames?name=sillerenb%C3%BChl",
-  {
-    method: "GET",
-    headers: {
-      Authorization: WEATHER_API_TOKEN,
-      Accept: "application/json",
-    },
-  }
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const locationsDiv = document.getElementById("locations");
-
-    data.forEach((location) => {
-      const locationDiv = document.createElement("div");
-      locationDiv.classList.add("location");
-
-      locationDiv.innerHTML = `
-                  <h2>${location.name}</h2>
-                  <p><strong>Höhe:</strong> ${location.height} m ü.M.</p>
-                  <p><strong>Geolocation:</strong> [${location.geolocation.lat}, ${location.geolocation.lon}]</p>
-                  <p><strong>Geolocation:</strong> ${location.geolocation.id}</p>
-              `;
-
-      // Schleife durch die geolocation_names für weitere Orte
-      location.geolocation.geolocation_names.forEach((geoName) => {
-        const geoNameDiv = document.createElement("div");
-        geoNameDiv.classList.add("geolocation-name");
-        geoNameDiv.innerHTML = `
-                      <p>${geoName.name} (${geoName.plz}), ${geoName.province}, ${geoName.height} m ü.M.</p>
-                  `;
-        locationDiv.appendChild(geoNameDiv);
-      });
-
-      locationsDiv.appendChild(locationDiv);
-    });
-  })
-  .catch((error) => {
-    console.error("Es gab ein Problem mit der API-Anfrage:", error);
-  }); */
